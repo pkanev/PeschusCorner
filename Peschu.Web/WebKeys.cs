7dd357865f39ebdfbef899a45c5379b96d21092b
pkanev <pvkanev@gmail.com>
2018-04-30 10:15:46 +0300

    Empty web keys

---
+++ Peschu.Web/WebKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace Peschu.Web
+{
+    public static class WebKeys
+    {
+        public const string ConnectionString = "";
+        public const string SeedAdminEmail = "";
+        public const string SeedAdminPassword = "";
+    }
+}
