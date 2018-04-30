01ac431f7e8c2e995d01d86aab7884ddede1214b
pkanev <pvkanev@gmail.com>
2018-04-30 10:13:42 +0300

    added tags filter

--- Peschu.Web/wwwroot/js/site.js
+++ Peschu.Web/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿// Write your JavaScript code.
+﻿(function () {
+    $(document).ready(function () {
+        $('#tags-filter').keyup(function () {
+            searchTags($('#tags-filter').val().toLowerCase());
+        });
+    });
+
+    function searchTags(searchTerm) {
+        $('.tags-list').each(function (index, element) {
+            var tag = $(element).text();
+            if (!tag.toLowerCase().includes(searchTerm)) {
+                $(element).hide();
+            } else {
+                $(element).show();
+            }
+        });
+    };
+})(); 
